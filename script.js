a909dd9af0c458ea6310f125f281d8ec202b2ae3
AnwarSadek <amd28686@gmail.com>
2025-10-21 14:45:19 +0600

    Anwar_Sadek

---
+++ script.js
@@ -0,0 +1,35 @@
+// this is a practice script file
+
+
+
+// let namta1 = 8;
+
+// for (let c=1; c<=18; c++) {
+//     console.log(namta1*c);
+// }
+
+// let idk = "aaaaa"; 
+// for ( let c=0; c<=5; c++) {
+//     console.log(idk);
+// }
+
+// let namta1 = 12;
+
+// for (let c=12; c>=1; c--) {
+//     console.log(namta1*c);
+// }
+
+// let idk = "*"; 
+// for ( let c=1; c< 6; c++) {
+//     console.log(idk);
+// }
+
+// let a = "1";
+// let b = 1;
+// let c = a+b;
+// console.log(c);
+
+let Rafi = " ";
+for ( let bia = 1; bia <= 4; bia++) {
+    Rafi+=(bia+"wife \n");
+} console.log("Rafi bia korbe"+Rafi);
